9c71c91003b49b4ad921d9af951cee1a2843e6f1
Дмитро Скороходов <dimon.skorohodov@gmail.com>
2024-02-28 14:14:46 +0200

    im stupid and i fix maybe that

--- app.cjs
+++ app.cjs
@@ -77,7 +77,7 @@ app.post("https://food-order-app-backend-smoky.vercel.app/orders", async (req, r
 });
 
 
-app.get("food-order-app-backend-smoky.vercel.app", (req,res,next) => {
+app.get("https://food-order-app-backend-smoky.vercel.app", (req,res,next) => {
   res.status(200).json({message: "ITS WORKING!"});
 })
 
